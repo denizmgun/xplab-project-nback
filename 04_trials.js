6869ad10193a1027c1c6659e0023438bb4ca16be
denizmguen <dguen@uos.de>
2019-07-29 00:19:56 +0200

    Adapted trial Sizes and Probabilites

--- 04_trials.js
+++ 04_trials.js
@@ -13,8 +13,8 @@ const nback = {
 	practice_n4final: generate_nback(trials=20, n=4, pbait=(1/9),ptarget=0.25,
 	stimuli=[2,3,2,6,7,3,6,1,4,5,3,3,7,4,9,1,3,1,4,2]),
 	
-	trials_n3: generate_nback(trials=103, n=3, pbait=(2.55/10), ptarget=(1/10)),
-	trials_n4: generate_nback(trials=104, n=4, pbait=(2.5/10), ptarget=(1/10)),
+	trials_n3: generate_nback(trials=83, n=3, pbait=(2.5/10), ptarget=(1/10)),
+	trials_n4: generate_nback(trials=84, n=4, pbait=(2.5/10), ptarget=(1/10)),
 	control_trials_n3: generate_nback(trials=103, n=3, pbait=0, ptarget=0),
 	control_trials_n4: generate_nback(trials=104, n=4, pbait=0, ptarget=0)
     }
